8cfeb7d6dcde2eac6285af941b0cad44864132c4
avirageorgek <avirageorge@gmail.com>
2024-01-20 17:57:29 +0000

    Styling header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 
+import Header from "./components/Header";
+
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
