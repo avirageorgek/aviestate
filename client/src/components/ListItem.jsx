e2c7434129d31dce8848ff1ac572ff6cdf7a2d3c
avirageorge <aviracasino@gmail.com>
2024-02-17 12:35:54 +0000

    Lsiting page: added images in list component

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -10,10 +10,11 @@ const ListItem = (props) =>{
     }
 
     return (
-        <div className="rounded-lg border-solid border-2 border-black-900 p-4 my-2 flex flex-row">
-            <p className="w-5/6 text-black-500">{props.listItem.name}</p>
-            <div className="flex flex-row gap-2 w-1/6">
-                <Link><FaRegEdit className="text-green-700"/></Link>
+        <div className="rounded-lg border-solid border-2 border-black-900 p-4 my-2 flex flex-row bg-slate-200">
+            <img class="min-w-12 min-h-12 max-w-16 max-h-16 rounded" src={props.listItem.imageUrls[0]} alt="List image"></img>
+            <p className="w-5/6 text-black-500 mx-4 self-center">{props.listItem.name}</p>
+            <div className="flex flex-row gap-2 w-1/6 self-center">
+                <Link><FaRegEdit className="text-green-600"/></Link>
                 <FaTrash className="text-red-700" onClick={deleteItem} />
             </div>
         </div>
